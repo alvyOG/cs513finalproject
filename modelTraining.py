5916f14f1457b3a846bdad3a676db83d554be35a
Chrisg27 <chrisg2706@gmail.com>
2022-11-18 17:56:31 -0500

    Added file to create and export models.

--- modelTraining.py
+++ modelTraining.py
@@ -1,11 +1,14 @@
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
+import xgboost as xgb
+import pickle
 from sklearn.metrics import accuracy_score
 
 def manual_separation(bad_line):
     right_split = bad_line[:-2] + [",".join(bad_line[-2:])]
     return right_split
+
 def training():
     trainData = pd.read_csv("trainset.csv", on_bad_lines= manual_separation, engine="python")
     testData = pd.read_csv("testset.csv", on_bad_lines= manual_separation, engine="python")
@@ -25,7 +28,26 @@ def training():
     prediction = rf.predict(xTest)
     print(f"Random Forest Classifier Test accuracy: {accuracy_score(yTest, prediction)}")
 
+    filename = "RandomForestModel.sav"
+    pickle.dump(rf, open(filename, 'wb'))
+    print("Random Forest is saved to file")
+
     reg = LogisticRegression(penalty='l2')
     reg.fit(xTrain, yTrain)
     prediction = reg.predict(xTest)
     print(f"Logistic Regression Test accuracy: {accuracy_score(yTest, prediction)}")
+
+    filename = "LogisticRegressionModel.sav"
+    pickle.dump(reg, open(filename, 'wb'))
+    print("Logistic Regression is saved to file")
+
+    xgb_classifier = xgb.XGBClassifier()
+    xgb_classifier.fit(xTrain, yTrain)
+    prediction = xgb_classifier.predict(xTest)
+    print(f"XGboost Test accuracy: {accuracy_score(yTest, prediction)}")
+
+    filename = "XGboostModel.sav"
+    pickle.dump(xgb_classifier, open(filename, 'wb'))
+    print("XGBoost is saved to file")
+
+training()
