025fdd2bc41f0a6a733a7744af9c054815238f73
Chrisg27 <chrisg2706@gmail.com>
2022-12-02 16:32:22 -0500

    Added resource gathering for modelTesting.py

--- modelTesting.py
+++ modelTesting.py
@@ -1,9 +1,18 @@
 import pandas as pd
 import pickle
+import os
 
 import time
+import psutil
+import multiprocessing as mp
 
-def test(filename):
+def runTest(dataset, model, runTime):
+    start = time.perf_counter()
+    model.predict(dataset)
+    end = time.perf_counter()
+    runTime.value = end-start
+
+def readInFile(filename):
     testData = pd.read_csv("testset.csv")
     print("File read in.")
 
@@ -13,10 +22,18 @@ def test(filename):
 
     xTest = testData.drop('malicious', axis=1)
 
-    start = time.perf_counter()
-    prediction = model.predict(xTest)
-    end = time.perf_counter()
-    print(prediction)
-    print(end-start)
+    runTime = mp.Value('f', 0.0)
+    runningTest = mp.Process(target=runTest, args=(xTest, model, runTime))
+    runningTest.start()
+    process = psutil.Process(pid=runningTest.pid)
 
-test("XGboostModel.sav")
+    while runningTest.exitcode == None:
+        print(f'Parent: {os.getpid()}. Child usage: {process.cpu_percent()}.')
+        time.sleep(0.1)
+
+    runningTest.join()
+    print(f'Process runtime: {runTime.value}')
+
+if __name__ == '__main__':
+    mp.set_start_method('spawn')
+    readInFile("XGboostModel.sav")
