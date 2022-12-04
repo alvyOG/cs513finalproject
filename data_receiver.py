8983c03b507c634c05d030f158c5203d9d58bf7e
Alex <aralvarez023@gmail.com>
2022-12-04 15:52:58 -0500

    add send/receive of botnet batch data

--- data_receiver.py
+++ data_receiver.py
@@ -1,6 +1,13 @@
 import sys
 import socket
 import signal
+import pandas as pd
+import pickle
+import os
+import time
+import psutil
+import multiprocessing as mp
+from io import BytesIO
 
 RECV_BUFFER_SIZE = 4096
 QUEUE_LENGTH = 10
@@ -11,9 +18,35 @@ def breakHandler(signum,frame):
     global isBreak
     isBreak = True
 
-def server(server_port):
+def runPrediction(dataset, model, runTime):
+    start = time.perf_counter()
+    model.predict(dataset)
+    end = time.perf_counter()
+    runTime.value = end-start
+
+def initModel(filename):
+    file = open(filename, 'rb')
+    model = pickle.load(file)
+    file.close()
+    return model
+
+def runTest(model, datajson):
+    testData = pd.read_json(datajson)
+    runTime = mp.Value('f', 0.0)
+    runningPrediction = mp.Process(target=runPrediction, args=(testData, model, runTime))
+    runningPrediction.start()
+    process = psutil.Process(pid=runningPrediction.pid)
+
+    while runningPrediction.exitcode == None:
+        print(f'Parent: {os.getpid()}. Child usage: {process.cpu_percent()}.')
+        time.sleep(0.1)
+
+    runningPrediction.join()
+    print(f'Process runtime: {runTime.value}')
+
+def server(server_ip, server_port, model):
     # Open socket
-    HOST = '127.0.0.1'
+    HOST = server_ip
     PORT = server_port
     global isBreak
     signal.signal(signal.SIGINT, breakHandler)
@@ -29,23 +62,29 @@ def server(server_port):
         while True:
             # Receive data on the socket until no data left or buffer full
             data = c.recv(RECV_BUFFER_SIZE)
-            datasplit = data.split(',')
+            datajson = pd.read_csv(BytesIO(data), names=[
+                "ip.src",
+                "ip.dst",
+                "ip.proto",
+                "frame.len",
+            ])
             if not data:
                 break
             else:
-                # If there is data write to stdout and flush
-                sys.stdout.buffer.write(data)
-                sys.stdout.buffer.flush()
+                runTest(model, datajson)
         # Close connection once all the data is read from that connection
         c.close()
     # Close the socket when server receives a kill signal
     s.close()
     
 def main():
-    if len(sys.argv) != 2:
-        sys.exit("Usage: server-python.py [Server Port]")
-    server_port = int(sys.argv[1])
-    server(server_port)
+    if len(sys.argv) != 3:
+        sys.exit("Usage: data_receiver.py [Server IP] [Server Port]")
+    server_ip = sys.argv[1]
+    server_port = int(sys.argv[2])
+    model = initModel("XGboostModel.sav")
+    mp.set_start_method('spawn')
+    server(server_ip, server_port, model)
 
 if __name__ == "__main__":
     main()
--- data_sender.py
+++ data_sender.py
@@ -20,6 +20,20 @@ def sender(server_ip, server_port, dataset, batch_size, message_count):
     # When connected, open data set and send batches
     while True:
         # send data
+        csvfile = open(dataset, newline='\n')
+        csvreader = csv.reader(csvfile)
+        # remove headers
+        headers = csvreader.__next__()
+        msgcounter = 0
+        while msgcounter < message_count:
+            message = ""
+            batchcounter = 0
+            while batchcounter < batch_size:
+                message += ','.join(csvreader.__next__())+','
+                batchcounter += 1
+            s.send(message[0:len(message)-1])
+            msgcounter += 1
+        csvfile.close()
         break
     # close the socket when done sending
     s.close()
@@ -42,14 +56,14 @@ def printSendData(dataset, batch_size, message_count):
 
 def main():
     if len(sys.argv) != 6:
-        sys.exit("Usage: data_sender.py <IP> <PORT> <DATASET CSV> <BATCH SIZE> <MSG COUNT>")
+        sys.exit("Usage: data_sender.py [IP] [Port] [foo.csv] [Batch Size] [Msg Count]")
     server_ip = sys.argv[1]
     server_port = int(sys.argv[2])
     dataset = sys.argv[3]
     batch_size = int(sys.argv[4])
     message_count = int(sys.argv[5])
-    #sender(server_ip, server_port, dataset, batch_size, message_count)
-    printSendData(dataset, batch_size, message_count)
+    sender(server_ip, server_port, dataset, batch_size, message_count)
+    #printSendData(dataset, batch_size, message_count)
 
 if __name__ == "__main__":
     main()
